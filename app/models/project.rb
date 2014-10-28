7270a32b678c3b46de99a4581a6f0f6dc6fe71a0
Ashley <a.clarkem@gmail.com>
2014-10-28 15:13:16 -0600

    can view all projects

---
+++ app/models/project.rb
@@ -0,0 +1,30 @@
+class Project
+
+  attr_reader :name
+
+  def initialize(project_json)
+    @name = project_json["name"]
+  end
+
+  def self.get_projects
+
+    conn = Faraday.new(:url => 'https://www.pivotaltracker.com') do |faraday|
+      faraday.request :url_encoded # form-encode POST params
+      faraday.response :logger # log requests to STDOUT
+      faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+    end
+    conn.headers["X-TrackerToken"]=ENV["TRACKER_TOKEN"]
+    conn.headers["Accepts"]="application/xml"
+    response = conn.get 'services/v5/projects' # GET http://sushi.com/nigiri/sake.json
+    response.body
+
+  end
+
+  def self.all
+    projects_json = JSON.parse(get_projects)
+    projects_json.map do |project_json|
+      Project.new(project_json)
+    end
+  end
+
+end
